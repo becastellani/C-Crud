d90bb4aa7204209b08b8fdaeb1e342223d9562c3
Bernardo Mantoan Castellani <bernardo.castellani@alunos.bpkedu.com.br>
2023-03-29 20:59:07 -0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace Aula1006
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAmigo = new System.Windows.Forms.DataGridView();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtApelido = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAmigo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAmigo
+            // 
+            this.dgvAmigo.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.dgvAmigo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAmigo.Location = new System.Drawing.Point(37, 170);
+            this.dgvAmigo.Name = "dgvAmigo";
+            this.dgvAmigo.Size = new System.Drawing.Size(650, 245);
+            this.dgvAmigo.TabIndex = 0;
+            this.dgvAmigo.DoubleClick += new System.EventHandler(this.dgvAmigo_DoubleClick);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPesquisar.Location = new System.Drawing.Point(575, 421);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(112, 34);
+            this.btnPesquisar.TabIndex = 5;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(34, 13);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigo.TabIndex = 2;
+            this.lblCodigo.Text = "Código";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apelido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(198, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Email";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Enabled = false;
+            this.txtCodigo.Location = new System.Drawing.Point(37, 29);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // txtApelido
+            // 
+            this.txtApelido.Location = new System.Drawing.Point(37, 78);
+            this.txtApelido.Name = "txtApelido";
+            this.txtApelido.Size = new System.Drawing.Size(100, 20);
+            this.txtApelido.TabIndex = 2;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(201, 29);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(438, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(201, 78);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(438, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalvar.Location = new System.Drawing.Point(552, 104);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(87, 32);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(458, 109);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 6;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(729, 542);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtApelido);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.dgvAmigo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAmigo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAmigo;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtApelido;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnExcluir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Aula1006
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexao = new MySqlConnection(
+                "Persist Security Info=False; SSLmode = none;"+
+                "Server = 127.0.0.1;"+
+                "Port=3305;"+
+                "Database=dbprojeto;"+
+                "Uid = root;"+
+                "Pwd = usbw;"
+                );
+            conexao.Open();
+            MySqlCommand sql = new MySqlCommand(
+                "SELECT * from tblamigo;", conexao);
+            MySqlDataAdapter da = new MySqlDataAdapter(sql);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conexao.Close();
+            dgvAmigo.DataSource = dt;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexao = new MySqlConnection(
+                "Persist Security Info=False; SSLmode = none;" +
+                "Server = 127.0.0.1;" +
+                "Port=3305;" +
+                "Database=dbprojeto;" +
+                "Uid = root;" +
+                "Pwd = usbw;"
+                );
+            conexao.Open();
+            string comandoSql;
+            if (txtCodigo.Text == String.Empty)
+            {
+                comandoSql =
+                    "insert into tblamigo(nome, apelido, email) values" + "('" + txtNome.Text + "','" + txtApelido.Text + "','" + txtEmail.Text + "');";
+            }
+            else
+            {
+                comandoSql = "update tblamigo set nome = '" + txtNome.Text + "', apelido = '" + txtApelido.Text + "', email = '"+txtEmail.Text + "' where idAmigo = " + txtCodigo.Text;
+            }
+
+            MySqlCommand sql = new MySqlCommand(
+                comandoSql, conexao);
+            sql.ExecuteNonQuery();
+            conexao.Close();
+            txtCodigo.Clear();
+            txtNome.Clear();
+            txtApelido.Clear();
+            txtEmail.Clear();
+            MessageBox.Show("Salvo com sucesso", "Salvo");
+        }
+
+
+
+        private void dgvAmigo_DoubleClick(object sender, EventArgs e)
+        { 
+                txtCodigo.Text =
+                    dgvAmigo.CurrentRow.Cells[0].Value.ToString();
+                txtNome.Text =
+                    dgvAmigo.CurrentRow.Cells[1].Value.ToString();
+                txtApelido.Text =
+                    dgvAmigo.CurrentRow.Cells[2].Value.ToString();
+                txtEmail.Text =
+                dgvAmigo.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+             if(txtCodigo.Text != String.Empty)
+            {
+                MySqlConnection conexao = new MySqlConnection(
+                "Persist Security Info=False; SSLmode = none;" +
+                "Server = 127.0.0.1;" +
+                "Port=3305;" +
+                "Database=dbprojeto;" +
+                "Uid = root;" +
+                "Pwd = usbw;"
+                );
+                conexao.Open();
+
+                MySqlCommand sql = new MySqlCommand("delete from tblamigo " +
+                    "where idAmigo = " + txtCodigo.Text, conexao);
+                sql.ExecuteNonQuery();
+                conexao.Close();
+                MessageBox.Show("Excluido com Sucesso", "Excluído");
+            }
+
+        }
+    }
+}
